739b647f6970f316d46af845c11d984b85c2434a
Noel Vincent <noelvincent@me.com>
2024-09-28 14:24:58 +0900

    change fetch func to es5 compatible version

--- script.js
+++ script.js
@@ -6,13 +6,25 @@ inputTest = document.createElement("h1");
 document.body.append(inputTest);
 inputTest.textContent = "Input Test";
 
-async function fetchWords() {
-  let promise = await fetch("words.json");
-  let data = await promise.json();
-  allWords = data;
-  wordTest.textContent = data.join(", ");
-  // let word_array = await promise.json()
-  // return word_array
+// async function fetchWords() {
+//   let promise = await fetch("words.json");
+//   let data = await promise.json();
+//   allWords = data;
+//   wordTest.textContent = data.join(", ");
+// }
+
+function fetchWords() {
+  fetch("words.json")
+    .then(function (promise) {
+      return promise.json();  // Convert the response to JSON
+    })
+    .then(function (data) {
+      allWords = data;  // Assign data to `allWords`
+      wordTest.textContent = data.join(", ");  // Update the content of `wordTest`
+    })
+    .catch(function (error) {
+      console.error('Error:', error);  // Handle any errors that occur
+    });
 }
 
 let allWords = [];
