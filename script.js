6d46b487a295e07620e206340c958c8961f4e181
Noel Vincent <noelvincent@me.com>
2024-09-28 14:17:43 +0900

    add input testing; update background

--- script.js
+++ script.js
@@ -1,15 +1,24 @@
-test = document.createElement("textarea");
-document.body.append(test);
-test.textContent = "himom";
+wordTest = document.createElement("textarea");
+document.body.append(wordTest);
+wordTest.textContent = "himom";
+
+inputTest = document.createElement("h1");
+document.body.append(inputTest);
+inputTest.textContent = "Input Test";
 
 async function fetchWords() {
   let promise = await fetch("words.json");
   let data = await promise.json();
   allWords = data;
-  test.textContent = data.join(", ");
+  wordTest.textContent = data.join(", ");
   // let word_array = await promise.json()
   // return word_array
 }
 
 let allWords = [];
 fetchWords();
+
+document.addEventListener("keydown", (event) => {
+  console.log("event.key: ", event.key);
+  inputTest.textContent = event.key
+});
